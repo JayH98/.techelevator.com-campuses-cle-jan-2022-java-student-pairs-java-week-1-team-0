3bcf47c880c42e13817397e30e4adc91e3f17e16
Angel Scott <angel.v.scott@gmail.com>
2022-01-21 20:46:34 -0500

    Finished TempConvert exercise

--- exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,8 +1,23 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		/* Scanner scanner = new Scanner(System.in);
+		System.out.println("PLease enter the number you want converted to binary.");
+		// String decimal = scanner.nextLine();
+		int decimalToBinary = Integer.parseInt(scanner.nextLine());
+			int[] remainders = new int[];
+
+		while (decimalToBinary > 0) {
+
+		}
+		//for (int i = decimalToBinary; i > 0;) {
+		}
+		// System.out.print(decimalToBinary); */
+
 
 	}
 
--- exercise/src/main/java/com/techelevator/TempConvert.java
+++ exercise/src/main/java/com/techelevator/TempConvert.java
@@ -1,8 +1,33 @@
 package com.techelevator;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter your temperature: ");
+		// String temp = scanner.nextLine();
+		double temp = Double.parseDouble(scanner.nextLine());
+		System.out.print("Is this temperature Celsius(C) or Farenheit(F)? ");
+		String tempType = scanner.nextLine();
+		double toFarenheit = temp * 1.8 + 32;
+		double toCelsius = (temp - 32) / 1.8;
+
+
+
+		if (tempType.equals("C")) {
+			double temperatureInCelsius = toFarenheit;
+			System.out.println("The temperature is " + temperatureInCelsius + "F");
+		}
+		else if (tempType.equals("F")) {
+			double temperatureInFarenheit = toCelsius;
+			System.out.println("The temperature is " + temperatureInFarenheit + "C");
+		}
+		else {
+			System.out.println("Input must be capital F or capital C. Try again!");
+		}
 
 	}
 
