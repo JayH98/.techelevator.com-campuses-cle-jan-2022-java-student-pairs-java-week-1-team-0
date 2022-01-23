a01522295acdadc259a22573905f8678389260ca
Angel Scott <angel.v.scott@gmail.com>
2022-01-23 15:14:13 -0500

    Nearly finished with DecimalToBinary

--- exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -5,39 +5,32 @@ import java.util.Scanner;
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
-
-		 Scanner scanner = new Scanner(System.in);
-
+		Scanner scanner = new Scanner(System.in);
 		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
-		 String userValue = scanner.nextLine();
-		System.out.println(userValue);
+		String userValues = scanner.nextLine();
+		String[] decimalValues = userValues.split(" ");
+		int[] decimalValuesInInt = new int[decimalValues.length];
 
-		String [] storeQuotient = userValue.split(" ");
-
-		int[] decimals = new int [storeQuotient.length];
-
-		for (int i = 0; i < storeQuotient.length; i++) {
-			decimals[i] = Integer.parseInt(storeQuotient[i]);
-
-
+		for (int i = 0; i < decimalValues.length; i++) {
+			decimalValuesInInt[i] = Integer.parseInt(decimalValues[i]);
 		}
+		//int quotient = 0;
 
+		for (int i = 0; i < decimalValuesInInt.length; i++) {
+			int quotient = decimalValuesInInt[i];
+			System.out.print(+decimalValuesInInt[i] + " in binary is ");
 
 			do {
-				int remainder = decimals[0] % 2;
-				decimals[0] = decimals[0] / 2;
-				System.out.print(remainder + " ");
-
-			}
-			while (decimals[0] > 0);
-
-
-//		if (userValue <= 0) {
-//			System.out.println("Input must be a positive number!");
-//			break;
+				int quotientRemainder = quotient % 2;
+				quotient = quotient / 2;
+				System.out.print(quotientRemainder);
+			} while (quotient > 0);
+			System.out.println();
 		}
 
+//
 
+	}
 
 	}
 
